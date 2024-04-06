8b9677e9cb6aa60af6b19c0c563c1709e2ab5c8a
Eden Guzman Andia <eden.guzman@niceincontact.com>
2024-04-06 08:18:07 -0400

    Bug fixing images

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -9,7 +9,7 @@ export const HeroCard = ({
     first_appearance,
     characters
 }) => {
-    const heroImageUrl = `/assets/${id}.jpg`
+    const heroImageUrl = `/heroes/${id}.jpg`
   return (
     <div className='col animate__animated animate__fadeIn'>
         <div className="card">
